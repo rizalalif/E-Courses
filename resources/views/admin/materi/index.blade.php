ca357ac62c53f83c8e827a4e207247b02e379520
faridahmd1 <163585905+faridahmd1@users.noreply.github.com>
2024-08-08 17:08:32 +0700

    nothing change

--- resources/views/admin/materi/index.blade.php
+++ resources/views/admin/materi/index.blade.php
@@ -27,14 +27,15 @@
                         </div>
                         <div
                             class="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
-                            <button type="button"
+                            <button type="button" id="defaultModalButton" data-modal-target="defaultModal"
+                                data-modal-toggle="defaultModal"
                                 class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
                                 <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20"
                                     xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                     <path clip-rule="evenodd" fill-rule="evenodd"
                                         d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
                                 </svg>
-                                Tambah materi
+                                Tambah Materi
                             </button>
                             <div class="flex items-center w-full space-x-3 md:w-auto">
 
