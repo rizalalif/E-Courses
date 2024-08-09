2be1dfe99f0a2b18633b93bae3bc2f7f43c5d1be
faridahmd1 <163585905+faridahmd1@users.noreply.github.com>
2024-08-09 23:23:52 +0700

    feat: material management

--- app/Models/Materi.php
+++ app/Models/Materi.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Materi extends Model
 {
     use HasFactory;
+    use HasUlids;
+
+    protected $fillable=[
+        'name',
+        'status',
+        'description'
+    ];
+
+    public function paket_detail():HasOne
+    {
+        return $this->hasOne(MateriDetail::class);
+    }
+
+    // public function paket():MorphOne
+    // {
+    //     return $this->morphOne(Paket::class,'paketable');
+    // }
 }
